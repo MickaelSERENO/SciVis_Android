453229cd4a94ed8b79871f18f3cc19944dd1452c
Sereno Mickaël <serenomickael@gmail.com>
2021-03-26 14:50:25 +0100

    Update, add fullscreen support

--- app/src/main/java/com/sereno/vfv/MainActivity.java
+++ app/src/main/java/com/sereno/vfv/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
